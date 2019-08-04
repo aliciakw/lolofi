0c723427dcf8171bd478d78368c54f5f0cd36ee8
aliciakw <alicia.willett@gmail.com>
2019-08-03 21:14:09 -0400

    read pixels as an array

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
-// import jimp from 'jimp';
+import jimp from 'jimp';
 import fs from 'fs';
+import EXIFParser from 'exif-parser';
 
-async function main() {
-  // const image = await jimp.read('test.jpg');
-  // console.log(image);
-  // image.color([{ apply: 'red', params: [100] }]);
+const _background = 0x00000000;
+const filename = 'test_sm.jpg'; //args[0];
+const _defaultPixel = { r: 0, g: 0, b: 0, a: 255 };
+
+function getPixelRgb(image, x, y) {
+  const hex = image.getPixelColor(x, x);
+  return jimp.intToRGBA(hex);
 }
 
-// main();
 
-const path = './test.jpg'; //args[0];
-console.log('path: ' + path);
-fs.readFile(path, (err, data) => {
-  if (err) throw err;
-  /* data will be a buffer */
-  console.log(data);
-});
+async function main() {
+  const image = await jimp.read(filename);
+  const w = image.getWidth();
+  const h = image.getHeight();
+
+  const pixelRBGs = new Array(100).fill(_defaultPixel);
+  for (let y = 0; y < h; y++) {
+    for (let x = 0; x < w; x++) {
+      pixelRBGs[y * w + x] = getPixelRgb(image, x, y);
+    }
+  }
+  console.log(pixelRBGs);
+}
+
+main();
