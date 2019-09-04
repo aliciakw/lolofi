4f6a97a34c9c51f4101f2d5227abc0f521f0921d
Alicia Willett <alicia@sanctuary.computer>
2019-09-04 08:23:48 -0400

    resize output images and delete high res

--- lo2fi.sh
+++ lo2fi.sh
@@ -2,14 +2,14 @@
 # Example usasge 
 # /bin/bash lo2fi.sh data_raw/A.Girl.Walks.Home.Alone.at.Night.2014.720p.BRRip.900MB.MkvCage.mkv a_girl
 # 
-echo .....................................
-echo .....................................
-echo ...........making extra lofi.........
-echo .....................................
+echo ......................................
+echo ......................................
+echo ..........making xxxtra lofi..........
+echo ......................................
 
 VIDEOFILE=$1
 NAMESPACE=$2
-FRAMES=30
+FRAMES=3
 interval=1
 
 echo $VIDEOFILE
@@ -22,9 +22,11 @@ for ((i=1; i<=FRAMES; i++)); do
     minute=($i * $interval)
     timestamp="00:$minute:00"
     output_filename="$output_dir/$minute.png"
+    output_filename_sm="$output_dir/$minute-sm.png"
     echo "$timestamp --> $output_filename"
     $(ffmpeg -ss $timestamp -i $VIDEOFILE -frames:v 1 $output_filename)
-
+    $(convert -resize 10x $output_filename $output_filename_sm)
+    $(rm $output_filename)
 done
 
 echo .....................................
